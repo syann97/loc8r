13f0beb27027b4d9f54cf76205c0053a032e582b
syann97 <shallom75321@gmail.com>
2025-04-14 13:11:08 +0900

    fix: Google API Key exposed on GitHub

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose');
-//const dbURI = 'mongodb://localhost/Loc8r';
-var dbURI = 'mongodb+srv://my_atlas_user:d4865123@cluster0.qjlik09.mongodb.net/Loc8r';
+const dbURI = 'mongodb://localhost/Loc8r';
+
 mongoose.connect(dbURI, { useNewUrlParser: true });
 
 mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected to ${dbURI}`);
+  console.log(`Mongoose connected to ${dbURI}`);
 });
-mongoose.connection.on('error', err => {
-    console.log(`Mongoose connection error: ${err}`);
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connection error: ${err}`);
 });
 mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+  console.log('Mongoose disconnected');
 });
 const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close(() => {
-        console.log(`Mongoose disconnected through 
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected through 
 ${msg}`);
-        callback();
-    });
+    callback();
+  });
 };
 
 // For nodemon restarts
 process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
-    });
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
 });
 // For app termination
 process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
-        process.exit(0);
-    });
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
 });
 // For Heroku app termination
 process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
-        process.exit(0);
-    });
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
 });
 
 require('./locations');
